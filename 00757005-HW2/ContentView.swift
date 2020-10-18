80e0be56f79db4874a2ca5a4b43f4b089559f508
Starmy <mf99250@gmail.com>
2020-10-18 14:06:29 +0800

    Initial Commit

---
+++ 00757005-HW2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00757005-HW2
+//
+//  Created by User19 on 2020/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 00757005-HW2/_0757005_HW2App.swift
@@ -0,0 +1,17 @@
+//
+//  _0757005_HW2App.swift
+//  00757005-HW2
+//
+//  Created by User19 on 2020/10/18.
+//
+
+import SwiftUI
+
+@main
+struct _0757005_HW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
